624f1297807bbab626f02a6eef076b51df6f4d66
test <a@b.com>
2020-01-19 12:15:52 +0300

    test

--- task1/src/task1.cpp
+++ task1/src/task1.cpp
@@ -16,17 +16,18 @@ int main (int argc, char** argv) {
 		temp_vect.push_back(temp_string);
 	}
 	file1.close();
-	double data[temp_vect.size()];
-	for (size_t i=0; i < temp_vect.size(); i++) {
+	int size = temp_vect.size();
+	double data[size];
+	for (int i=0; i < size; i++) {
 		data[i] = boost::lexical_cast<double> (temp_vect[i]);
 	}
 	cout.precision(2);
 	cout << fixed;
-	gsl_sort(data, 1, 10);
-	cout << gsl_stats_quantile_from_sorted_data(data, 1, 10, 0.9) << endl;
-	cout << gsl_stats_median_from_sorted_data(data, 1, 10) << endl;
-	cout << gsl_stats_max(data, 1, 10) << endl;
-	cout << gsl_stats_min(data, 1, 10) << endl;
-	cout << gsl_stats_mean(data, 1, 10) << endl;
+	gsl_sort(data, 1, size);
+	cout << gsl_stats_quantile_from_sorted_data(data, 1, size, 0.9) << endl;
+	cout << gsl_stats_median_from_sorted_data(data, 1, size) << endl;
+	cout << gsl_stats_max(data, 1, size) << endl;
+	cout << gsl_stats_min(data, 1, size) << endl;
+	cout << gsl_stats_mean(data, 1, size) << endl;
 	return 0;
 }
